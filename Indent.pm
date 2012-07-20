9dab06bab1657d73afcfcc0f0c30f8ef44ef994e
Michal Špaček <tupinek@gmail.com>
2012-07-20 02:08:51 +0200

    Add 'strict_instruction' constructor parameter.

--- Indent.pm
+++ Indent.pm
@@ -648,7 +648,7 @@ __END__
 =item * C<preserved>
 
  TODO
- Default is reference to blank array.
+ Default value is reference to blank array.
 
 =item * C<raw_callback>
 
@@ -660,7 +660,12 @@ __END__
 =item * C<skip_bad_tags>
 
  TODO
- Default is 0.
+ Default value is 0.
+
+=item * C<strict_instruction>
+
+ TODO
+ Default value is 1.
 
 =back
 
