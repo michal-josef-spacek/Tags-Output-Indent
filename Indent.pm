3b2c38d65787b534bf3e2c2d20e35ce9eea3267e
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2026-03-31 21:09:08 +0200

    Prepare for 0.14 version

--- Indent.pm
+++ Indent.pm
@@ -19,7 +19,7 @@ Readonly::Scalar my $LAST_INDEX => -1;
 Readonly::Scalar my $LINE_SIZE => 79;
 Readonly::Scalar my $SPACE => q{ };
 
-our $VERSION = 0.13;
+our $VERSION = 0.14;
 
 # Finalize Tags output.
 sub finalize {
@@ -896,6 +896,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.13
+0.14
 
 =cut
